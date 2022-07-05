1ee547ca36c088abccdb4577d3b5332a2e0271bc
shubhamkanchar <shubhamkanchar687@gmail.com>
2022-07-05 19:52:28 +0530

    title changed

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -4,7 +4,7 @@
 <meta name="description" content="">
 <meta name="author" content="">
 
-<title>SB Admin 2 - Dashboard</title>
+<title>{{ config('app.name', 'fusion') }} - Dashboard</title>
 
 <!-- Custom fonts for this template-->
 <link href="{{url('public/theme/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
