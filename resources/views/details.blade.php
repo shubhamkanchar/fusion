6c72c92a18b6ac486f55c5c50d2d073bdc196ab8
shubhamkanchar <shubhamkanchar687@gmail.com>
2022-07-07 19:43:52 +0530

    fees section added

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -21,14 +21,14 @@
             <div class="col-lg-6 col-md-12">
                 <img style="width: 100%;" src="{{ url('public/uploads/course/').'/'.$course->file }}" alt="{{ $course->name ?? '' }}">
             </div>
-            <div class="col-lg-6 col-md-12 mb-5">
+            <div class="col-lg-6 col-md-12 mt-5">
                 <h2>What is {{ $course->name ?? '' }} ?</h2>
                 <p>{{ $course->desc ?? '' }}</p>
-                <!-- <h4 class="mt-5">Fees : {{ $course->fees }} only</h4> -->
+                <h4 class="mt-3">Fees : {{ $course->fees }} only</h4>
                 <h4>Duration : {{ $course->duration }} Months</h4>
             </div>
 
-            <div class="col-lg-6 col-md-12 mt-5">
+            <div class="col-lg-6 col-md-12 mt-3">
                 <h2>Syllabus</h2>
                 @php $i=1; @endphp
                 @foreach( explode(',',$course->syllabus) as $d )
@@ -41,7 +41,7 @@
                 <h4 class="mt-5">Fees : {{ $course->fees }} only</h4>
                 <h4>Duration : {{ $course->duration }} Months</h4>
             </div> -->
-            <div class="col-lg-6 col-md-12 mt-5">
+            <div class="col-lg-6 col-md-12 mt-2">
                 <h2>Tools</h2>
                 @php
                 $i=1;
